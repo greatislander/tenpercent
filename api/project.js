30ba889a11427de323aef78ff86332602e672978
Ned Zimmerman <ned@bight.dev>
2020-08-02 19:04:39 -0300

    feat: default to all resources if none is specified

--- api/project.js
+++ api/project.js
@@ -13,11 +13,13 @@ module.exports = async (request, response) => {
 	}
 
 	if (!resource) {
-		resource = 0;
+		resource = 'all';
 	}
 
+	const endpoint = (resource === 'all') ? `https://api.transifex.com/organizations/${organization}/projects/${project}/resources/` : `https://api.transifex.com/organizations/${organization}/projects/${project}/resources/${resource}`;
+
 	const data = await fetch(
-		`https://api.transifex.com/organizations/${organization}/projects/${project}/resources/`,
+		endpoint,
 		{
 			headers: {
 				Authorization: 'Basic ' + Buffer.from(`api:${token}`, 'utf8').toString('base64')
@@ -31,16 +33,34 @@ module.exports = async (request, response) => {
 	} else if (data.status === 404) {
 		response.status(404).send('The organization or project could not be found.');
 	} else if (data.status === 200) {
-		if (Object.prototype.hasOwnProperty.call(json, resource)) {
+		if (resource === 'all') {
+			let stringCount = 0;
+			let translatedStringCount = 0;
+
+			for (const resource of json) {
+				stringCount += resource.stringcount;
+				translatedStringCount += resource.stringcount * resource.stats.translated.percentage;
+			}
+
+			response.status(200).send({
+				status: Number(
+					(translatedStringCount / stringCount * 100).toFixed(2)
+				)
+			});
+		} else {
+			let stringCount = 0;
+
+			for (const element of Object.values(json.stats)) {
+				stringCount += element.translated.stringcount;
+			}
+
 			const translated = Number(
-				(json[resource].stats.translated.percentage * 100).toFixed(2)
+				(stringCount / (Object.values(json.stats).length * json.stringcount) * 100).toFixed(2)
 			);
-			response.setHeader('Cache-Control', 's-maxage=86400');
+
 			response.status(200).send({
 				status: translated
 			});
-		} else {
-			response.status(404).send('The resource could not be found.');
 		}
 	}
 };
